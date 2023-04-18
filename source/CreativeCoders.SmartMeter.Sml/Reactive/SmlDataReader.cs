5e121545770b31b0cf36f858ec24ba19ccf22c7a
darthsharp <48331467+darthsharp@users.noreply.github.com>
2023-04-18 22:05:43 +0200

    Rewrite SML reading and parsing in a reactive style

---
+++ source/CreativeCoders.SmartMeter.Sml/Reactive/SmlDataReader.cs
@@ -0,0 +1,31 @@
+﻿namespace CreativeCoders.SmartMeter.Sml.Reactive;
+
+public class SmlDataReader
+{
+    private static readonly byte[] DocBeginSeq = {
+        EscapeChar, EscapeChar, EscapeChar, EscapeChar,
+        DocBeginChar, DocBeginChar, DocBeginChar, DocBeginChar
+    };
+    
+    private const byte EscapeChar = 0x1B;
+
+    private const byte DocBeginChar = 0x01;
+    
+    private readonly List<byte> _currentBlock = new List<byte>();
+
+    private readonly List<byte> _buffer = new List<byte>();
+
+    private SmlReadDataMode _currentMode = SmlReadDataMode.WaitForBegin;
+
+    private Action<SmlMessage> _handleMessage;
+
+    public void AddHandler(Action<SmlMessage> handleMessage)
+    {
+        _handleMessage = handleMessage;
+    }
+
+    public void Parse(byte[] data)
+    {
+        
+    }
+}
---
+++ source/CreativeCoders.SmartMeter.Sml/Reactive/SmlReactiveExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace CreativeCoders.SmartMeter.Sml.Reactive;
+
+public static class SmlReactiveExtensions
+{
+    public static IObservable<SmlMessage> ReadSmlMessages(this IObservable<byte[]> observable)
+    {
+        var messageSubject = new Subject<SmlMessage>();
+        
+        var smlDataReader = new SmlDataReader();
+        
+        smlDataReader.AddHandler(msg => messageSubject.OnNext(msg));
+
+        observable.Subscribe(data => smlDataReader.Parse(data));
+
+        return messageSubject;
+    }
+
+    public static IObservable<SmlValue> ReadSmlValues(this IObservable<SmlMessage> observable)
+    {
+        var valueReader = new SmlValueReader();
+
+        return observable
+            .SelectMany(msg => valueReader.Read(msg.Data));
+    }
+}
